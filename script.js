2a7ca54352b1cbc305142a1fee76cf392ab3b871
Zunira-Sajjad <zunairasajjad124@gmail.com>
2025-08-20 00:56:13 +0500

    change button text & header text

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ window.loadUserTemplates = async function () {
 
           <div class="button-actions">
             <button class="order-design-btn" onclick="orderDesign('${newTemplateId}')">
-              Order Design
+              Send Letter
             </button>
             <button class="edit-btn" onclick="openEditModal('${newTemplateId}')">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
